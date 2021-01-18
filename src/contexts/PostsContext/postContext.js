b2ce850fbc3c24d647bdb8f923c8202290006e65
Muneeb Ur Rehman <muneeb.github@gmail.com>
2021-01-18 23:02:59 +0500

    added docs of postContext and utils

--- src/contexts/PostsContext/postContext.js
+++ src/contexts/PostsContext/postContext.js
@@ -19,49 +19,77 @@ export function PostsContextProvider(props) {
   const [dates, setDates] = useState([]);
   const [showAll, setShowAll] = useState(true);
 
+  /**
+   * gets returned post data from server cleans that and updates post state with it
+   * @param response- response returned from get request to server for posts
+   */
   function updatePostsFromResponse(response) {
+    //get required data and put it in right format
     const cleanedPosts = cleanPostsData(response);
+    //update posts state and filtered state for initial start of page
     setPosts(cleanedPosts);
     setFilteredPosts(cleanedPosts);
   }
 
+  /**
+   * removes one post from filtered posts
+   * @param postID - id of the post to be removed
+   */
   function removePostFromFilteredPosts(postID) {
-    console.log("removing post with", postID);
     const newFilteredPosts = filteredPosts.filter(e => e.id !== postID);
     setFilteredPosts(newFilteredPosts);
-    console.log(newFilteredPosts);
   }
 
+  /**
+   * gets a post from all posts {posts state} and adds that in the filtered post
+   * @param postID- post id of the post to be copied from all posts
+   */
   function importPostFromFilteredPosts(postID) {
-    console.log("importing post with", postID);
     const indexOfPostToAdd = posts.findIndex(e => e.id === postID);
     let newPosts = [...filteredPosts, posts[indexOfPostToAdd]];
     newPosts = [...new Set(newPosts)];
     setFilteredPosts(newPosts);
-    console.log(newPosts);
   }
 
+  /**
+   * appends given posts to the previous posts in state
+   * @param newPosts- posts to be appended
+   */
   function appendPosts(newPosts) {
     setPosts((previousPosts) => [...previousPosts, ...newPosts]);
   }
 
+  /**
+   * appends given markers with existing ones in the state
+   * @param newMarkers- new markers to be appended
+   */
   function appendMarkers(newMarkers) {
     let allMarkers = [...markers, ...newMarkers];
     allMarkers = [...new Set(allMarkers)];
     setMarkers(allMarkers);
   }
 
+  /**
+   * gets markers from response returned from server and appends them to existing markers
+   * @param response- response from server
+   */
   function updateMarkersFromResponse(response) {
     const cleanedMarkers = cleanMarkersData(response);
     appendMarkers(cleanedMarkers);
   }
 
-  function resetFilteredPosts(){
-    setFilteredPosts([])
+  /**
+   * resets the filtered posts to empty array
+   */
+  function resetFilteredPosts() {
+    setFilteredPosts([]);
   }
 
-  function populateFilteredPostsWithAll(){
-    setFilteredPosts(posts)
+  /**
+   * set filteredPosts state with all posts
+   */
+  function populateFilteredPostsWithAll() {
+    setFilteredPosts(posts);
   }
 
   return (
